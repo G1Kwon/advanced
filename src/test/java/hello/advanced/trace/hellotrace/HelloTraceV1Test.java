f2eaee071bc7a3cb093e8ec75480ded4cbef9d61
g1kwon <spring_shot@naver.com>
2022-07-10 10:17:01 +0900

    log tracing V2 - 적용

--- src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -3,8 +3,6 @@ package hello.advanced.trace.hellotrace;
 import hello.advanced.trace.TraceStatus;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class HelloTraceV1Test {
 
     @Test
