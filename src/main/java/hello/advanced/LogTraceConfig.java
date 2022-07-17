3fce6c94d98d1f24c3bc3c03010852e0b273ae65
g1kwon <spring_shot@naver.com>
2022-07-17 13:44:29 +0900

    템플릿 콜백 패턴 - 적용_2

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -1,6 +1,5 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FiledLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
--- src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
+++ src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
@@ -1,28 +1,27 @@
-package hello.advanced.app.v4;
+package hello.advanced.app.v5;
 
+import hello.advanced.trace.callback.TraceTemplate;
 import hello.advanced.trace.logtrace.LogTrace;
-import hello.advanced.trace.template.AbstractTemplate;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
-public class OrderRepositoryV4 {
+public class OrderRepositoryV5 {
 
-    private final LogTrace trace;
+    private final TraceTemplate template;
+
+    public OrderRepositoryV5(LogTrace trace) {
+        this.template = new TraceTemplate(trace);
+    }
+
     public void save(String itemId) {
 
-        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
-            @Override
-            protected Void call() {
+        template.execute("OrderRepository.save()", () -> {
                 if (itemId.equals("ex")) {
                     throw new IllegalStateException("예외 발생!");
                 }
                 sleep(1000);
                 return null;
-            }
-        };
-        template.execute("OrderRepository.save()");
+        });
     }
     private void sleep (int millis) {
         try {
